c5e0d8e4cb339c01d124574f3cc103322e78cf15
Sky2walkerss <starw8169@gmail.com>
2022-01-31 13:47:34 +0200

    final header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+//dropdown menu catalog
 let menuBtn = document.querySelector('.catalog');
 
 let menu = document.querySelector('.burger_nav')
 menuBtn.addEventListener('click',function () {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
+})
+
+//dropdown menu shoping cart
+let basket__hovering = document.querySelector('.basket__hovering');
+let shopping__cart = document.querySelector('.shopping__cart');
+let clc__cart_add = document.querySelector('.clc__cart_add');
+shopping__cart.addEventListener('click',function(){
+  shopping__cart.classList.toggle('active');
+  basket__hovering.classList.toggle('active');
+  clc__cart_add.classList.toggle('active');
+
 })
