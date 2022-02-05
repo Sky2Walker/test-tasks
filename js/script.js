7a7dd9d74bf03e8b1fd16a83d2d3a66bbf4904ab
Sky2walkerss <starw8169@gmail.com>
2022-02-05 14:41:18 +0200

    accordeon

--- js/script.js
+++ js/script.js
@@ -17,3 +17,20 @@ shopping__cart.addEventListener('click',function(){
   clc__cart_add.classList.toggle('active');
 
 })
+
+
+//accordion
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
